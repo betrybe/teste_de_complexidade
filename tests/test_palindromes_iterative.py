ed131d41c211b3a35da6913fc806b3aa5d0d29d1
“brunobatista” <brunobatista101@gmail.com>
2021-01-04 18:52:43 -0300

    gabarito1

--- tests/test_palindromes_iterative.py
+++ tests/test_palindromes_iterative.py
@@ -25,10 +25,10 @@ def test_validar_tempo_iterative():
     expect = ("from challenges.challenge_palindromes_iterative "
               "import is_palindrome_iterative")
     print("printou o tempo de iterative")
-    print(timeit.timeit(f'is_palindrome_iterative("ANA")',
+    print(timeit.timeit('is_palindrome_iterative("ANA")',
                         setup=f"{expect}"))
     print("printou o tempo de iterative")
     print("printou o tempo de iterative com varias execucoes")
-    print(timeit.timeit(f'is_palindrome_iterative("ANA")',
+    print(timeit.timeit('is_palindrome_iterative("ANA")',
                         setup=f"{expect}", number=10000000))
     print("printou o tempo de iterative com varias execucoes")

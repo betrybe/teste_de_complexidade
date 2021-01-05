607f98a853dbd59392fbf5270a6043573bd75690
“brunobatista” <brunobatista101@gmail.com>
2021-01-05 16:10:54 -0300

    ajuste flake

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,36 +1,38 @@
 def selectionSort(alist):
-   for fillslot in range(len(alist)-1,0,-1):
-       positionOfMax=0
-       for location in range(1,fillslot+1):
-           if alist[location]>alist[positionOfMax]:
-               positionOfMax = location
+    for fillslot in range(len(alist)-1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, fillslot+1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location
 
-       temp = alist[fillslot]
-       alist[fillslot] = alist[positionOfMax]
-       alist[positionOfMax] = temp
-    
+        temp = alist[fillslot]
+        alist[fillslot] = alist[positionOfMax]
+        alist[positionOfMax] = temp
+
+
 def insertionSort(alist):
-   for index in range(1,len(alist)):
+    for index in range(1, len(alist)):
+        currentvalue = alist[index]
+        position = index
 
-     currentvalue = alist[index]
-     position = index
+        while position > 0 and alist[position-1] > currentvalue:
+            alist[position] = alist[position-1]
+            position = position-1
 
-     while position>0 and alist[position-1]>currentvalue:
-         alist[position]=alist[position-1]
-         position = position-1
+    alist[position] = currentvalue
 
-     alist[position]=currentvalue
 
 def bubbleSort(alist):
-    for passnum in range(len(alist)-1,0,-1):
+    for passnum in range(len(alist)-1, 0, -1):
         for i in range(passnum):
-            if alist[i]>alist[i+1]:
+            if alist[i] > alist[i+1]:
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
 
+
 def mergeSort(alist):
-    if len(alist)>1:
+    if len(alist) > 1:
         mid = len(alist)//2
         lefthalf = alist[:mid]
         righthalf = alist[mid:]
@@ -38,67 +40,61 @@ def mergeSort(alist):
         mergeSort(lefthalf)
         mergeSort(righthalf)
 
-        i=0
-        j=0
-        k=0
+        i = 0
+        j = 0
+        k = 0
         while i < len(lefthalf) and j < len(righthalf):
             if lefthalf[i] < righthalf[j]:
-                alist[k]=lefthalf[i]
-                i=i+1
+                alist[k] = lefthalf[i]
+                i = i+1
             else:
-                alist[k]=righthalf[j]
-                j=j+1
-            k=k+1
+                alist[k] = righthalf[j]
+                j = j+1
+            k = k+1
 
         while i < len(lefthalf):
-            alist[k]=lefthalf[i]
-            i=i+1
-            k=k+1
+            alist[k] = lefthalf[i]
+            i = i+1
+            k = k+1
 
         while j < len(righthalf):
-            alist[k]=righthalf[j]
-            j=j+1
-            k=k+1
+            alist[k] = righthalf[j]
+            j = j+1
+            k = k+1
+
+
 def quickSort(alist):
-   quickSortHelper(alist,0,len(alist)-1)
-
-def quickSortHelper(alist,first,last):
-   if first<last:
-
-       splitpoint = partition(alist,first,last)
-
-       quickSortHelper(alist,first,splitpoint-1)
-       quickSortHelper(alist,splitpoint+1,last)
+    quickSortHelper(alist, 0, len(alist)-1)
 
 
-def partition(alist,first,last):
-   pivotvalue = alist[first]
-
-   leftmark = first+1
-   rightmark = last
-
-   done = False
-   while not done:
-
-       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
-           leftmark = leftmark + 1
-
-       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
-           rightmark = rightmark -1
-
-       if rightmark < leftmark:
-           done = True
-       else:
-           temp = alist[leftmark]
-           alist[leftmark] = alist[rightmark]
-           alist[rightmark] = temp
-
-   temp = alist[first]
-   alist[first] = alist[rightmark]
-   alist[rightmark] = temp
+def quickSortHelper(alist, first, last):
+    if first < last:
+        splitpoint = partition(alist, first, last)
+        quickSortHelper(alist, first, splitpoint-1)
+        quickSortHelper(alist, splitpoint+1, last)
 
 
-   return rightmark
+def partition(alist, first, last):
+    pivotvalue = alist[first]
+    leftmark = first+1
+    rightmark = last
+    done = False
+    while not done:
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark - 1
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+    return rightmark
+
 
 def sort(array):
     """Sort the array by using quicksort."""
@@ -117,10 +113,12 @@ def sort(array):
             elif x > pivot:
                 greater.append(x)
         # Don't forget to return something!
-        return sort(less)+equal+sort(greater)  # Just use the + operator to join lists
+        return sort(less)+equal+sort(greater)
     # Note that you want equal ^^^^^ not pivot
-    else:  # You need to handle the part at the end of the recursion - when you only have one element in your array, just return the array.
+    else:
         return array
+
+
 def is_anagram_selectionSort(first_string, second_string):
     if len(first_string) != len(list(second_string)):
         return False
@@ -129,6 +127,7 @@ def is_anagram_selectionSort(first_string, second_string):
     second_sorted = selectionSort(list(second_string))
 
     return first_sorted == second_sorted
+
 
 def is_anagram_insertionSort(first_string, second_string):
     if len(first_string) != len(second_string):
@@ -139,6 +138,7 @@ def is_anagram_insertionSort(first_string, second_string):
 
     return first_sorted == second_sorted
 
+
 def is_anagram_bubbleSort(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
@@ -147,6 +147,7 @@ def is_anagram_bubbleSort(first_string, second_string):
     second_sorted = bubbleSort(list(second_string))
 
     return first_sorted == second_sorted
+
 
 def is_anagram_mergeSort(first_string, second_string):
     if len(first_string) != len(second_string):
@@ -157,6 +158,7 @@ def is_anagram_mergeSort(first_string, second_string):
 
     return first_sorted == second_sorted
 
+
 def is_anagram_quickSort(first_string, second_string):
     if len(first_string) != len(list(second_string)):
         return False
@@ -165,6 +167,7 @@ def is_anagram_quickSort(first_string, second_string):
     second_sorted = quickSort(list(second_string))
 
     return first_sorted == second_sorted
+
 
 def is_anagram_sort(first_string, second_string):
     if len(first_string) != len(second_string):
--- tests/test_anagrams.py
+++ tests/test_anagrams.py
@@ -4,34 +4,37 @@ import timeit
 def test_validar_tempo_anagrama():
     expect = ("from challenges.challenge_anagrams "
               "import is_anagram_selectionSort, is_anagram_insertionSort, "
-              "is_anagram_bubbleSort, is_anagram_mergeSort, is_anagram_quickSort, "
+              "is_anagram_bubbleSort, is_anagram_mergeSort, "
+              "is_anagram_quickSort, "
               "is_anagram_sort")
     print("is_anagram_selectionSort")
     teste = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
-             "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+             "sed do eiusmod tempor incididunt ut "
+             "labore et dolore magna aliqua.")
     teste2 = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
-              "do sed eiusmod tempor incididunt ut labore et dolore magna aliqua.")
-    print(timeit.timeit(f'is_anagram_selectionSort("{teste}", "{teste2}")',
-                        setup=f"{expect}", number=10000)) 
-    print(timeit.repeat(f'is_anagram_selectionSort("{teste}", "{teste2}")',
-                        setup=f"{expect}", number=10000, repeat=5)) 
+              "do sed eiusmod tempor incididunt "
+              "ut labore et dolore magna aliqua.")
+    print(timeit.timeit(f'is_anagram_selectionSort("{teste}","{teste2}")',
+                        setup=f"{expect}", number=10000))
+    print(timeit.repeat(f'is_anagram_selectionSort("{teste}","{teste2}")',
+                        setup=f"{expect}", number=10000, repeat=5))
     print("is_anagram_insertionSort")
     print(timeit.timeit(f'is_anagram_insertionSort("{teste}", "{teste2}")',
                         setup=f"{expect}", number=10000))
     print(timeit.repeat(f'is_anagram_insertionSort("{teste}", "{teste2}")',
-                        setup=f"{expect}", number=10000, repeat=5)) 
+                        setup=f"{expect}", number=10000, repeat=5))
     print("is_anagram_bubbleSort")
     print(timeit.timeit(f'is_anagram_bubbleSort("{teste}", "{teste2}")',
                         setup=f"{expect}", number=10000))
     print(timeit.repeat(f'is_anagram_bubbleSort("{teste}", "{teste2}")',
-                        setup=f"{expect}", number=10000, repeat=5)) 
+                        setup=f"{expect}", number=10000, repeat=5))
     print("is_anagram_mergeSort")
     print(timeit.timeit(f'is_anagram_mergeSort("{teste}", "{teste2}")',
                         setup=f"{expect}", number=10000))
     print(timeit.repeat(f'is_anagram_mergeSort("{teste}", "{teste2}")',
-                        setup=f"{expect}", number=10000, repeat=5)) 
+                        setup=f"{expect}", number=10000, repeat=5))
     print("is_anagram_sort")
     print(timeit.timeit(f'is_anagram_sort("{teste}", "{teste2}")',
                         setup=f"{expect}", number=10000))
     print(timeit.repeat(f'is_anagram_sort("{teste}", "{teste2}")',
-                        setup=f"{expect}", number=10000, repeat=5)) 
+                        setup=f"{expect}", number=10000, repeat=5))
